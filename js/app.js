6d74086f9bf1fead4e1e73dbefb700f821e53940
carlos8881 <yoyo19990801@gmail.com>
2024-06-11 23:49:29 +0800

    v3.5

--- js/app.js
+++ js/app.js
@@ -4,6 +4,13 @@ new Vue({
         categories: [
             { name: '筆記', link: 'category_note.html' },
             { name: '自傳', link: '#' },
+        ],
+        tags: [
+            { name: 'AI', link: '#' },
+            { name: 'GIT', link: '#' },
+            { name: '好文連結', link: '#' },
+            { name: '筆記', link: '#' },
+            { name: '自傳', link: '#' },
         ]
     }
 });
