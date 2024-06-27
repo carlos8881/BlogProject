8aad9d58a16106e69205e735a7cf3af69d1659f5
carlos8881 <yoyo19990801@gmail.com>
2024-06-28 04:09:51 +0800

    v6.4

--- message_app/server.js
+++ message_app/server.js
@@ -62,7 +62,6 @@ app.get('/comments', async (req, res) => {
     }
 });
 
-
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
